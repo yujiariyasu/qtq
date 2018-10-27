0bd27fe6665b134ebb119d9f10445c19f509ade1
yujiariyasu <yuji.ariyasu@gmail.com>
2018-10-27 17:23:42 +0900

    add hyde button to header

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,12 @@
 class UsersController < ApplicationController
   def show
-  end
+    category = [1,3,5,7]
+    current_quantity = [1000,5000,3000,8000]
 
-  def edit
-  end
-
-  def update
-  end
-
-  def update_params
+    @graph = LazyHighCharts::HighChart.new('graph') do |f|
+      f.title(text: 'ItemXXXの在庫の推移')
+      f.xAxis(categories: category)
+      f.series(name: '在庫数', data: current_quantity)
+    end
   end
 end
