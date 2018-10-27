6b57f8c21fe527fb58cda58e18142eccae0dcfb7
yujiariyasu <yuji.ariyasu@gmail.com>
2018-10-27 13:40:46 +0900

    add sidebar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
+//= require highcharts/highcharts
+//= require highcharts/highcharts-more
+//= require highcharts/highstock
 //= require turbolinks
 //= require_tree .
