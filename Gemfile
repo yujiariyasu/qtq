f49842bcc2b20bd7ef2b760a72fa0c292cdff170
yujiariyasu <yuji.ariyasu@gmail.com>
2018-10-29 19:44:23 +0900

    fix

--- Gemfile
+++ Gemfile
@@ -17,6 +17,8 @@ gem 'jbuilder', '~> 2.5'
 gem 'bootstrap-sass', '3.3.7'
 gem 'font-awesome-rails'
 gem 'lazy_high_charts'
+gem 'i18n'
+gem 'bcrypt',  '3.1.11'
 
 group :development, :test do
   gem 'byebug', platform: :mri
@@ -25,6 +27,7 @@ group :development, :test do
 end
 
 group :development do
+  gem 'better_errors', '2.1.1'
   gem 'web-console', '>= 3.3.0'
   gem 'listen', '~> 3.0.5'
   gem 'spring'
@@ -34,6 +37,9 @@ end
 group :test do
   gem 'factory_bot_rails'
   gem 'rspec-rails'
+  gem 'capybara', '2.5.0'
+  gem 'turnip', '2.1.0'
+  gem 'selenium-webdriver', '2.47.1'
 end
 
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,46 @@ class UsersController < ApplicationController
   def show
     category = [1,3,5,7]
     current_quantity = [1000,5000,3000,8000]
-
-    @graph = LazyHighCharts::HighChart.new('graph') do |f|
+    months = [ 4, 5, 6, 7, 8, 9 ]
+    product_A_sales = [ 1_000_000, 1_200_000, 1_300_000,
+      1_400_000, 1_200_000, 1_100_000 ]
+    product_B_sales = [   300_000,   500_000,   750_000,
+      1_150_000, 1_350_000, 1_600_000 ]
+    @chart = LazyHighCharts::HighChart.new('graph') do |f|
       f.title(text: 'ItemXXXの在庫の推移')
       f.xAxis(categories: category)
       f.series(name: '在庫数', data: current_quantity)
     end
+
+
+    @chart2 = LazyHighCharts::HighChart.new("graph") do |c|
+      c.title(text: "4-9月売上")
+      # X軸の名称を設定 '月'
+      c.xAxis(categories: months, title: {text: '月'})       # Y軸の名称を設定 '円'
+      c.yAxis(title: {text: '円'})
+      c.series(name: "A", data: product_A_sales)
+      c.series(name: "B", data: product_B_sales)
+      # 判例を右側に配置
+     c.legend(align: 'right', verticalAlign: 'top', 
+       x: -100, y: 180, layout: 'vertical')
+     # グラフの種類を「折れ線グラフ」から「棒グラフ」に変更
+     c.chart(type: "column")
+    end
+
+    @chart3 = LazyHighCharts::HighChart.new("graph") do |c|
+      c.title(text: "製品別上期売上")
+      c.series({
+        colorByPoint: true,
+        # ここでは各月の売上額合計をグラフの値とする
+        data: [{name: 'A',y: product_A_sales.reduce{|sum,e| sum + e}},
+               {name: 'B',y: product_B_sales.reduce{|sum,e| sum + e}}]
+      })
+      c.plotOptions(pie: {allowPointSelect: true,cursor: 'pointer',dataLabels: {
+          enabled: true, format: '{point.name}: {point.percentage:.1f} %',
+        }
+      })
+      # グラフの種類として「パイチャート」を指定
+      c.chart(type: "pie")
+   end
   end
 end
