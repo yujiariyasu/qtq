97eef9287c81baab5fd32d7747bc3f9486179a4b
yujiariyasu <yuji.ariyasu@gmail.com>
2018-11-02 22:07:14 +0900

    add current_user? method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -79,6 +79,6 @@ class UsersController < ApplicationController
 
     def correct_user
       @user = User.find(params[:id])
-      redirect_to root_url unless @user != current_user
+      redirect_to root_url unless current_user?(@user)
     end
 end
