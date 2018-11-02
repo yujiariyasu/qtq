6047a6ac1e7599d7703f76ac00be79f12b339943
yujiariyasu <yuji.ariyasu@gmail.com>
2018-11-02 10:09:40 +0900

    add user create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,4 +36,19 @@ class UsersController < ApplicationController
       c.chart(type: "pie")
    end
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
