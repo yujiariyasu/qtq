96817d7a7f988934be398b94b7b2bfe5630c039b
yujiariyasu <yuji.ariyasu@gmail.com>
2018-11-02 09:38:49 +0900

    add flash message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      redirect_to root_url
+    else
+      flash.now[:danger] = 'メールアドレスとパスワードの組み合わせが正しくありません。'
+      render 'new'
+    end
+  end
+
+  def destroy
+  	
+  end
 end
