eaca14ab815a2160188935e4786d26f96a5cf0e3
yujiariyasu <yuji.ariyasu@gmail.com>
2018-11-02 09:20:45 +0900

    add sessions routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  resources :users, only: [:show]
   root 'users#show'
+  resources :users, only: [:show]
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
