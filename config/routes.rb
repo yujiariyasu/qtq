ae1428ce07843fb31f71e2ae3c8db41ae454ed6b
yujiariyasu <yuji.ariyasu@gmail.com>
2018-11-03 00:21:04 +0900

    fix routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root 'users#show'
   resources :users, only: [:show]
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  root 'users#show'
 end
