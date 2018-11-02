06c45634260925bcdaf5ea327dacff96238c2caf
yujiariyasu <yuji.ariyasu@gmail.com>
2018-11-02 09:47:22 +0900

    add method current_user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      redirect_to root_url
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'メールアドレスとパスワードの組み合わせが正しくありません。'
       render 'new'
