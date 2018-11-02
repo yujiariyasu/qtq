e276f42b7caf2964953ec4cc2cb7d19e4b65683f
yujiariyasu <yuji.ariyasu@gmail.com>
2018-11-03 00:57:07 +0900

    fix for mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,10 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = 'localhost:3001' # ここをコピペすると失敗します。自分の環境に合わせてください。
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
 
   config.action_mailer.perform_caching = false
 
