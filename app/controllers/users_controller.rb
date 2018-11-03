6de2d928e4e4e3577d56d1111fbc13fb594a052a
yujiariyasu <yuji.ariyasu@gmail.com>
2018-11-03 21:37:04 +0900

    authenticated?メソッドの抽象化

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,9 +43,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      remember @user
-      redirect_to @user
+      UserMailer.account_activation(@user).deliver_now
+      flash[:info] = "ユーザー認証のためのメールを送信しました。"
+      redirect_to root_url
     else
       render 'new'
     end
