ec947e2c8318410050f5f7abb8527be489c7fb0a
yujiariyasu <yuji.ariyasu@gmail.com>
2018-11-03 11:13:22 +0900

    fix config about mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,9 +28,16 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :test
-  host = 'localhost:3001' # ここをコピペすると失敗します。自分の環境に合わせてください。
-  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port:                 587,
+    address:              'smtp.gmail.com',
+    domain:               'smtp.gmail.com',
+    user_name:            'ENV["MAIL_ADDRESS"]',
+    password:             'ENV["MAIL_PASS"]',
+    authentication:       'login',
+    enable_starttls_auto: true
+  }
 
   config.action_mailer.perform_caching = false
 
