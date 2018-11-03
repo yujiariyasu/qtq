f8089c7135143b904b0377b55973cab67bce3bcb
yujiariyasu <yuji.ariyasu@gmail.com>
2018-11-03 21:37:27 +0900

    add account_activation_controller edit

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,2 +1,15 @@
 class AccountActivationsController < ApplicationController
+
+  def edit
+    user = User.find_by(email: params[:email])
+    if user && !user.activated? && user.authenticated?(:activation, params[:id])
+      user.update_attribute(activated: true)
+      user.update_attribute(activated_at: Time.zone.now)
+      log_in user
+      redirect_to user
+    else
+      flash[:danger] = '無効なURLです。'
+      redirect_to root_url
+    end
+  end
 end
