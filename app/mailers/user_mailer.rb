0ff93375521020614b8f748e8b84cd5cc27f5304
yujiariyasu <yuji.ariyasu@gmail.com>
2018-11-03 11:13:45 +0900

    fix mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,20 +1,9 @@
 class UserMailer < ApplicationMailer
-
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.account_activation.subject
-  #
   def account_activation(user)
     @user = user
-    mail_to: @user.email, subject: 'RootedLearning：ユーザー認証メール'
+    mail to: user.email, subject: "RootedLearning：ユーザー認証メール"
   end
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.password_reset.subject
-  #
   def password_reset
     @greeting = "Hi"
 
