d898e2205e15541ca96fabc3b95f0b2d905b50b3
yujiariyasu <yuji.ariyasu@gmail.com>
2018-11-04 17:27:44 +0900

    add password_resets

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,7 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user, only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
+
   def new
   end
 
@@ -14,4 +17,20 @@ class PasswordResetsController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+  end
+
+  private
+
+    def get_user
+      @user = User.find_by(email: params[:email])
+    end
+
+    def valid_user
+      unless (@user && @user.activated? &&
+              @user.authenticated?(:reset, params[:id]))
+        redirect_to root_url
+      end
+    end
 end
