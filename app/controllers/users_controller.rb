945612ed90a9f2357d013cbf3c25139237228105
yujiariyasu <yuji.ariyasu@gmail.com>
2018-11-04 12:37:14 +0900

    add users#new action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,6 +40,10 @@ class UsersController < ApplicationController
    end
   end
 
+  def new
+    @user = User.new
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'users#show'
-  resources :users, only: [:show]
+  resources :users, only: [:show, :new, :create]
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
