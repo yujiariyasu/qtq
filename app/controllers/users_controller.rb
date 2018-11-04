7980f27ea0d9e224e749be420d0fab044b1bc99e
yujiariyasu <yuji.ariyasu@gmail.com>
2018-11-04 16:42:01 +0900

    logged_in_userメソッドを全コントローラーで使えるように

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -81,14 +81,6 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "ログインしてください。"
-        redirect_to login_url
-      end
-    end
-
     def correct_user
       @user = User.find(params[:id])
       redirect_to root_url unless current_user?(@user)
