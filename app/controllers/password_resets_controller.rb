ad222615b0999c4efb537ea894c8178c9c1c5d1e
yujiariyasu <yuji.ariyasu@gmail.com>
2018-11-04 16:56:16 +0900

    add password_resets#create

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -3,5 +3,15 @@ class PasswordResetsController < ApplicationController
   end
 
   def edit
+    @user = User.find_by(email: params[:password_reset][:email])
+    if @user
+      @user.create_reset_digest
+      @user.password_send_reset_email
+      flash[:info] = "メールを送信しました。"
+      redirect_to root_url
+    else
+      flash[:info] = "無効なメールアドレスです。"
+      render 'new'
+    end
   end
 end
