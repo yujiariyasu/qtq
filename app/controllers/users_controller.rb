ba3474e6e6866f5787c89f00424a29d789a05c08
yujiariyasu <yuji.ariyasu@gmail.com>
2018-11-05 20:29:11 +0900

    fix for user avatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action only: [:edit, :update]
+  before_action :logged_in_user,   only: [:edit, :update]
   before_action :correct_user,   only: [:edit, :update]
 
   def show
@@ -78,7 +78,7 @@ class UsersController < ApplicationController
 
   private
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :avatar)
     end
 
     def correct_user
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
+  mount_uploader :avatar, AvatarUploader
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
