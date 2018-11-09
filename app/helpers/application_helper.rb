54e150eb05f422083999739eca4b7254e9085a1e
yujiariyasu <yuji.ariyasu@gmail.com>
2018-11-09 22:56:19 +0900

    current_userをログインしていない箇所で使わないように

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def current_user_avatar_or
+    return logged_in? && current_user.avatar.file ? current_user.avatar :
+      "https://s3-ap-northeast-1.amazonaws.com/rootedlearning/uploads/default/avatar.jpg"
+  end
+
 end
