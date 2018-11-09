ef86240ff0a52c91cf8070a262d0084113378d62
yujiariyasu <yuji.ariyasu@gmail.com>
2018-11-09 15:41:57 +0900

    add columns to user

---
+++ db/migrate/20181109052358_add_provider_to_users.rb
@@ -0,0 +1,8 @@
+class AddProviderToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :provider, :string
+    add_column :users, :uid, :string
+    add_column :users, :oauth_token, :string
+    add_column :users, :oauth_expires_at, :datetime
+  end
+end
