a8c0dd9a9a6507e984ca7e686e060572bbc1053b
yujiariyasu <yuji.ariyasu@gmail.com>
2018-11-09 15:41:14 +0900

    add self.from_omniauth in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -59,13 +59,15 @@ class User < ApplicationRecord
   end
 
   def self.from_omniauth(auth)
-    user = User.find_by(email: auth.info.email) || Use.new
+    user = User.find_by(email: auth.info.email) || User.new
+    user.provider = auth.provider
     user.uid = auth.uid
-    user.name = auth.uid
+    user.name = auth.info.name
     user.email = auth.info.email
-    user.icon = auth.info.image
+    user.avatar = auth.info.image
     user.oauth_token = auth.credentials.token
     user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+    user
   end
 
   private
