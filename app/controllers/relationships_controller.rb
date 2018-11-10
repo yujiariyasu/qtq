65bd4d35b51c5af65e10e06b46bf2d9443434dd6
yujiariyasu <yuji.ariyasu@gmail.com>
2018-11-10 15:10:48 +0900

    add basic follow function

--- app/controllers/relationships_controller.rb
+++
@@ -1,9 +0,0 @@
-class RelationshipsController < ApplicationController
-  before_action :logged_in_user
-
-  def create
-  end
-
-  def destroy
-  end
-end
---
+++ app/controllers/user_relationships_controller.rb
@@ -0,0 +1,15 @@
+class UserRelationshipsController < ApplicationController
+  before_action :logged_in_user
+
+  def create
+    user = User.find(params[:followed_id])
+    current_user.follow(user)
+    redirect_to user
+  end
+
+  def destroy
+    user = UserRelationship.find(params[:id]).followed
+    current_user.unfollow(user)
+    redirect_to user
+  end
+end
