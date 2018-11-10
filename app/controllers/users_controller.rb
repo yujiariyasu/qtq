129ffd52b11712f66089adca2d87c52fd9b1d6ac
yujiariyasu <yuji.ariyasu@gmail.com>
2018-11-10 14:46:21 +0900

    add following/followers in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -89,6 +89,18 @@ class UsersController < ApplicationController
     end
   end
 
+  def following
+    @user = User.find(params[:id])
+    @users = @user.following
+    render 'show_follow'
+  end
+
+  def followers
+    @user = User.find(params[:id])
+    @users = @user.followers
+    render 'show_follow'
+  end
+
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation, :avatar)
