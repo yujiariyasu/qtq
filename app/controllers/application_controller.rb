4e196d9ce3188c71f338e94c1d6cda0c0e9c29e5
yujiariyasu <yuji.ariyasu@gmail.com>
2018-11-10 22:42:28 +0900

    エラーハンドリングをconcernに移す

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+
   def logged_in_user
     unless logged_in?
       store_location
@@ -12,24 +13,5 @@ class ApplicationController < ActionController::Base
   class Forbidden < ActionController::ActionControllerError;end
   class IpAddressRejected < ActionController::ActionControllerError; end
 
-  rescue_from Exception, with: :rescue500
-  rescue_from Forbidden, with: :rescue403
-  rescue_from IpAddressRejected, with: :rescue403
-  rescue_from ActionController::RoutingError, with: :rescue404
-  rescue_from ActiveRecord::RecordNotFound, with: :rescue404
-
-  def rescue403(e)
-    @exception = e
-    render 'errors/forbidden', status: 403
-  end
-
-  def rescue404(e)
-    @exception = e
-    render 'errors/not_found', status: 404
-  end
-
-  def rescue500(e)
-    @exception = e
-    render 'errors/internal_server_error', status: 500
-  end
+  include ErrorHandlers if Rails.env.production?
 end
