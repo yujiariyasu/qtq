680cc40ea033be428930ae13a5044023acb81991
yujiariyasu <yuji.ariyasu@gmail.com>
2018-11-17 10:51:41 +0900

    rm unuse file

--- app/assets/javascripts/footer.js
+++
@@ -1,7 +0,0 @@
-$(document).on('turbolinks:load', function() {
-  $(".animate-footer-span").on({"click":function(){
-    $(this).addClass("animated flip");
-  },"webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend":function(){
-    $(this).removeClass("animated flip");
-  }});
-});
