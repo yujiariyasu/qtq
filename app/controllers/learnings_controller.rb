885c6b95423abaffda96e434035d5998e4d21922
yujiariyasu <yuji.ariyasu@gmail.com>
2018-12-01 14:02:52 +0900

    add learning#new function

--- app/controllers/learnings_controller.rb
+++ app/controllers/learnings_controller.rb
@@ -1,16 +1,17 @@
 class LearningsController < ApplicationController
   def create
+
     @learning = Learning.new(learning_params)
     if @learning.save
       flash[:info] = '学習の登録しました。'
     else
       flash[:danger] = '学習の登録に失敗しました。'
     end
-    render params[:url]
+    redirect_to params[:url]
   end
 
   private
   def learning_params
-    params.require(learning).permit(:title, :description, :image).merge(use_id: current_user.id)
+    params.require(:learning).permit(:title, :description, :image).merge(user_id: current_user.id)
   end
 end
