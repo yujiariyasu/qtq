3921cce1d72a123f4f30b7144d978ffbe96f1cbc
yujiariyasu <yuji.ariyasu@gmail.com>
2018-12-01 17:58:48 +0900

    learning/showの基本機能完成

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
                                    dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
+  has_many :learnings, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
