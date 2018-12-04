a384c4ec5476a61b93118be0c3329edf79a7490d
yujiariyasu <yuji.ariyasu@gmail.com>
2018-12-04 19:51:10 +0900

    仮の復習日程算出アルゴリズム

--- app/controllers/learnings_controller.rb
+++ app/controllers/learnings_controller.rb
@@ -2,26 +2,45 @@ class LearningsController < ApplicationController
   def show
     @learning = Learning.find(params[:id])
     @user = @learning.user
-    date_category = (1..10).to_a.map{ |date| "#{date}" }
+    reviews = @learning.reviews
+    review_data = [100]
+    decrease_speed = 67
+    date_array = []
+    reviews.each do |review|
+      date_array << review.created_at.to_date.next_day
+    end
+    (@learning.created_at.to_date..reviews.last.created_at.to_date.next_day).each do |date|
+      if date_array.include?(date)
+        review_data << 100
+        next
+      end
+      last_data = review_date[-1] - decrease_speed
+      last_data = 0 if last_data < 0
+      review_data << last_data
+    end
+    review_data = [100, 70, 100, 80, 70, 60,-10,-20,-30,nil]
+    elapsed_days_num = reviews.present? ? @learning.elapsed_days_num : 1
+    range = elapsed_days_num > 9 ? 1..elapsed_days_num : 1..10
+    date_category = range.to_a.map{ |date| "#{date}日目" }
     text = 'そろそろ復習..'
-    # TODO: x軸の長さによって後ろに何日足すか変える
     @chart = LazyHighCharts::HighChart.new('graph') do |c|
       c.subtitle(text: text)
-      c.xAxis(labels: { format: '{value + 1}日目' })
+      c.xAxis(categories: date_category)
       c.yAxis(title: { text: nil },
               labels: { format: '{value}%' },
-              max: 100)
+              max: 100, min: 0)
       c.legend(layout: 'vertical', align: 'right', verticalAlign: 'top')
       c.plotOptions(line: { dataLabels: { enabled: true } },
         spline: {marker: {radius: 4,
                           lineColor: '#666666',
                           lineWidth: 1}})
-      c.series(type: 'spline', name: '定着率',
-               data: [100, 70, 100, 80, 70, 60,nil,nil,nil,nil]
+      c.series(type: 'spline', name: @learning.title,
+               data: review_data
       )
       c.chart(defaultSeriesType: "column")
       c.legend(maxHeight: 80)
-      c.tooltip(shared: true)
+      c.tooltip(shared: true,
+                pointFormat: '<b>{point.y} %</b>',)
     end
   end
 
--- app/models/learning.rb
+++ app/models/learning.rb
@@ -1,4 +1,8 @@
 class Learning < ApplicationRecord
   has_many :reviews, dependent: :destroy
   belongs_to :user
+
+  def elapsed_days_num
+    (created_at.to_date - reviews.order('date DESC').first.created_at.to_date).to_i
+  end
 end
