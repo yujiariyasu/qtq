f2ae48531ffe809fc13bb6c63ee3690d17fa1116
yujiariyasu <yuji.ariyasu@gmail.com>
2018-12-07 04:38:55 +0900

    add proficiency to review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,9 @@
 class ReviewsController < ApplicationController
   def create
     review = Review.new(review_params)
+      flash[:info] = '復習を記録しました。'
     unless review.save
-      flash[:danger] = '学習の登録に失敗しました。'
+      flash[:danger] = '復習の記録に失敗しました。'
     end
     redirect_to learning_url(params[:learning_id])
   end
