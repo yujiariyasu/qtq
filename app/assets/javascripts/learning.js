9a77c639b78dedfc8122832e7702e191407c7940
yujiariyasu <yuji.ariyasu@gmail.com>
2018-12-07 05:37:59 +0900

    複数image投稿ボタン追加

--- app/assets/javascripts/learning.js
+++ app/assets/javascripts/learning.js
@@ -2,4 +2,17 @@ $(document).on('turbolinks:load', function() {
   $("#review-slider").on("change", function() {
     $('#review-level').text($(this).val() + '%')
   });
+
+  $(".learning-upload-button").on("click", function() {
+    $("input[type=file]").click();
+  });
+
+  $('#learning-image-files').change(function() {
+    var fr = new FileReader();
+    fr.onload = function() {
+      var img = $('<img>').attr('src', fr.result).addClass('learning-image');
+      $('#learning-images-preview').append(img);
+    };
+    fr.readAsDataURL(this.files[0]);
+  });
 });
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181205154403) do
+ActiveRecord::Schema.define(version: 20181206203234) do
 
   create_table "learnings", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title"
     t.text     "description", limit: 65535
-    t.string   "image"
     t.integer  "user_id"
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.datetime "created_at",                                     null: false
+    t.datetime "updated_at",                                     null: false
+    t.string   "images",                    default: "--- []\n"
     t.index ["user_id"], name: "index_learnings_on_user_id", using: :btree
   end
 
