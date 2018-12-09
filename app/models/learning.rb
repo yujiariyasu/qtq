b5ad345075a12e6720eadb0a09784c414f87fcb5
yujiariyasu <yuji.ariyasu@gmail.com>
2018-12-09 23:47:13 +0900

    change default range

--- app/models/learning.rb
+++ app/models/learning.rb
@@ -6,18 +6,20 @@ class Learning < ApplicationRecord
   validates :title, presence: true, length: { maximum: 50 }
 
   def review_chart
-    review_data = [100]
+    review_data = [0, 100]
     decrease_speed = 67
     review_date_proficiency_map = set_review_date_proficiency
-    (created_at.to_date..Time.now.to_date).each do |date|
-      if review_date_proficiency_map.keys.include?(date)
-        review_data << 100
-        decrease_speed /= (1 + review_date_proficiency_map[date] * 2 / 100)
-        next
+    unless created_at.to_date == Time.now.to_date
+      (created_at.to_date..Time.now.to_date).each do |date|
+        if review_date_proficiency_map.keys.include?(date)
+          review_data << 100
+          decrease_speed /= (1 + review_date_proficiency_map[date] * 2 / 100)
+          next
+        end
+        last_data = review_data[-1] - decrease_speed
+        last_data = 0 if last_data < 0
+        review_data << last_data
       end
-      last_data = review_data[-1] - decrease_speed
-      last_data = 0 if last_data < 0
-      review_data << last_data
     end
     range = chart_date_range(review_date_proficiency_map)
     date_category = range.to_a.map{ |date| "#{date}日目" }
@@ -49,7 +51,7 @@ class Learning < ApplicationRecord
 
   def chart_date_range(review_date_proficiency_map)
     days_num = review_date_proficiency_map.present? ? elapsed_days_num : 1
-    days_num > 9 ? 1..elapsed_days_num : 1..10
+    days_num > 9 ? 0..elapsed_days_num : 0..10
   end
 
   def set_review_date_proficiency
@@ -61,19 +63,19 @@ class Learning < ApplicationRecord
   end
 
   def elapsed_days_num
-    (created_at.to_date - reviews.order('date DESC').first.created_at.to_date).to_i
+    (created_at.to_date - reviews.order('created_at desc').first.created_at.to_date).to_i
   end
 
   def review_text(point)
     return case point
     when 0..9
-      'ヤバいよヤバいよ..'
+      'ヤバいよヤバいよ'
     when 10..39
-      '頑張ろう..'
+      '頑張ろう'
     when 40..69
-      'そろそろ復習..'
+      'そろそろ復習'
     else
-      'イイ感じ..'
+      'イイ感じ'
     end
   end
 end
