71ecc2efbff7b0f952f36bcb6ec4e67c84675638
yujiariyasu <yuji.ariyasu@gmail.com>
2018-12-11 10:59:55 +0900

    markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,49 @@
 module ApplicationHelper
+  require "redcarpet"
+  require "coderay"
+
   def user_avatar_or_default(user)
     return user && user.avatar.file ? user.avatar :
       'https://s3-ap-northeast-1.amazonaws.com/rootedlearning/uploads/default/avatar.jpg'
+  end
+
+  class HTMLwithCoderay < Redcarpet::Render::HTML
+      def block_code(code, language)
+          language = language.split(':')[0]
+
+          case language.to_s
+          when 'rb'
+              lang = 'ruby'
+          when 'yml'
+              lang = 'yaml'
+          when 'css'
+              lang = 'css'
+          when 'html'
+              lang = 'html'
+          when ''
+              lang = 'md'
+          else
+              lang = language
+          end
+
+          CodeRay.scan(code, lang).div
+      end
+  end
+
+  def markdown(text)
+      html_render = HTMLwithCoderay.new(filter_html: true, hard_wrap: true)
+      options = {
+          autolink: true,
+          space_after_headers: true,
+          no_intra_emphasis: true,
+          fenced_code_blocks: true,
+          tables: true,
+          hard_wrap: true,
+          xhtml: true,
+          lax_html_blocks: true,
+          strikethrough: true
+      }
+      markdown = Redcarpet::Markdown.new(html_render, options)
+      markdown.render(text)
   end
 end
