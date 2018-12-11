851bd170276ee529ca37a7e0b5f9f57d162079f2
yujiariyasu <yuji.ariyasu@gmail.com>
2018-12-11 14:02:08 +0900

    fix js

--- app/assets/javascripts/learning.js
+++ app/assets/javascripts/learning.js
@@ -10,52 +10,52 @@ $(document).on('turbolinks:load', function() {
   $(".edit-learning-image-button").on("click", function() {
     $("#edit-learning-image-files").click();
   });
+  if(document.getElementById("learning-image-files") != null) {
+    document.getElementById('learning-image-files').onchange = function(event){
+      initializeFiles();
 
-  document.getElementById('learning-image-files').onchange = function(event){
-    initializeFiles();
+      var files = event.target.files;
 
-    var files = event.target.files;
+      for (var i = 0, f; f = files[i]; i++) {
+        var reader = new FileReader;
+        reader.readAsDataURL(f);
 
-    for (var i = 0, f; f = files[i]; i++) {
-      var reader = new FileReader;
-      reader.readAsDataURL(f);
-
-      reader.onload = (function(theFile) {
-        return function (e) {
-          var img = document.createElement('img');
-          img.className = 'learning-image';
-          img.src = e.target.result;
-          document.getElementById('learning-images-preview').insertBefore(img, null);
-        }
-      })(f);
-    }
+        reader.onload = (function(theFile) {
+          return function (e) {
+            var img = document.createElement('img');
+            img.className = 'learning-image';
+            img.src = e.target.result;
+            document.getElementById('learning-images-preview').insertBefore(img, null);
+          }
+        })(f);
+      }
+    };
   };
 
   function initializeFiles() {
     document.getElementById('learning-images-preview').innerHTML = '';
   }
+  if(document.getElementById("edit-learning-image-files") != null) {
+    document.getElementById('edit-learning-image-files').onchange = function(event){
+      initializeFiles();
 
-  document.getElementById('edit-learning-image-files').onchange = function(event){
-console.log(3);
-    initializeFiles();
+      var files = event.target.files;
 
-    var files = event.target.files;
+      for (var i = 0, f; f = files[i]; i++) {
+        var reader = new FileReader;
+        reader.readAsDataURL(f);
 
-    for (var i = 0, f; f = files[i]; i++) {
-      var reader = new FileReader;
-      reader.readAsDataURL(f);
-
-      reader.onload = (function(theFile) {
-        return function (e) {
-          var img = document.createElement('img');
-          img.className = 'learning-image';
-          img.src = e.target.result;
-          document.getElementById('edit-learning-images-preview').insertBefore(img, null);
-        }
-      })(f);
-    }
+        reader.onload = (function(theFile) {
+          return function (e) {
+            var img = document.createElement('img');
+            img.className = 'learning-image';
+            img.src = e.target.result;
+            document.getElementById('edit-learning-images-preview').insertBefore(img, null);
+          }
+        })(f);
+      }
+    };
   };
-
   function initializeFiles() {
     document.getElementById('edit-learning-images-preview').innerHTML = '';
   }
