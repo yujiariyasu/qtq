aac545435a7ea9da250007db358869573c02e62d
yujiariyasu <yuji.ariyasu@gmail.com>
2018-12-11 15:28:59 +0900

    add study_time to learning

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181206204605) do
+ActiveRecord::Schema.define(version: 20181211052149) do
 
   create_table "learnings", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20181206204605) do
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
     t.json     "images"
+    t.integer  "study_time"
     t.index ["user_id"], name: "index_learnings_on_user_id", using: :btree
   end
 
