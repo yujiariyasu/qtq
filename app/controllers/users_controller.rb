23583f7184902e26365dd8efe1397e7a467ae48b
yujiariyasu <yuji.ariyasu@gmail.com>
2018-12-13 17:58:05 +0900

    paramsのbugfix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,46 +7,8 @@ class UsersController < ApplicationController
   def show
     session[:path_info] = request.path_info
     @user = params[:id] ? User.find(params[:id]) : User.find(4)
-    category = (1..30).to_a
-    date_category = category.map{ |date| "#{date}日目" }
-    day_array = [4, 2, 4, 6, 1, 4, 2, 1, 5, 4, 3, 4, 2, 1, 5, 8, 4, 2, 4, 6, 1, 4, 2, 1, 5, 4, 3, 4, 2, 1]
-    days1 = day_array.sample(day_array.size)
-    days2 = days1.sample(days1.size)
-    days3 = days2.sample(days2.size)
-    text = 'いい調子!!'
-
-    @chart2 = LazyHighCharts::HighChart.new('graph') do |c|
-      c.chart(type: 'column')
-      c.subtitle(text: text)
-      c.xAxis( {
-        categories: date_category,
-        crosshair: true
-      })
-      c.tooltip( {
-        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-            '<td style="padding:0"><b>{point.y} 時間</b></td></tr>',
-        footerFormat: '</table>',
-        shared: true,
-        useHTML: true
-      })
-      c.plotOptions( {
-        column: {
-          pointPadding: 0.2,
-          borderWidth: 0
-        }
-      })
-      c.series(type: 'spline', name: '目標', data: [2]*30,
-               marker: {
-                 lineWidth: 1,
-                 lineColor: 'white',
-                 fillColor: 'white'
-               })
-      c.series(name: 'あなた', data: days1)
-      c.series(name: 'ライバル1', data: days2)
-      c.series(name: 'ライバル2', data: days3)
-    end
-    @chart3 = schedule_chart(@user)
+    @comparison_chart = comparison_chart(@user)
+    @schedule_chart = schedule_chart(@user)
   end
 
   def new
