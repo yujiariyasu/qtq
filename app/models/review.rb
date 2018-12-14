740acd90053d2d16467e93ab54377f70c2a0e45f
yujiariyasu <yuji.ariyasu@gmail.com>
2018-12-14 22:01:14 +0900

    bugfix

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,6 @@ class Review < ApplicationRecord
   belongs_to :learning
 
   def first_in_the_day?
-    Review.where(created_at: created_at.strftime('%Y-%m-%d 00:00:00')..created_at.strftime('%Y-%m-%d 23:59:59'), learning_id: learning_id).count == 1
+    Review.where(created_at: created_at.midnight..created_at.end_of_day, learning_id: learning_id).count == 1
   end
 end
