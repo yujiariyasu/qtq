dc1d91ad0d18a0d9c7d946a69321b5e96601ca34
yujiariyasu <yuji.ariyasu@gmail.com>
2018-12-15 15:08:34 +0900

    change domain

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,11 @@
 class UserMailer < ApplicationMailer
   def account_activation(user)
     @user = user
-    mail to: user.email, subject: 'RootedLearning：ユーザー認証メール'
+    mail to: user.email, subject: 'qtq：ユーザー認証メール'
   end
 
   def password_reset(user)
     @user = user
-    mail to: user.email, subject: 'RootedLearning：パスワードのリセット'
+    mail to: user.email, subject: 'qtq：パスワードのリセット'
   end
 end
