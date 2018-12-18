8e8a95f1fa5c56bbb76a157b177f28e4ba64ea92
yujiariyasu <yuji.ariyasu@gmail.com>
2018-12-18 20:10:29 +0900

    unicorn.sock->.unicorn.sock

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -23,7 +23,7 @@ stderr_path File.expand_path('../../log/unicorn_stderr.log', __FILE__)
 stdout_path File.expand_path('../../log/unicorn_stdout.log', __FILE__)
 
 # Nginxで使用する場合は以下の設定を行う。
-listen File.expand_path('../../tmp/sockets/unicorn.sock', __FILE__)
+listen File.expand_path('../../tmp/sockets/.unicorn.sock', __FILE__)
 # とりあえず起動して動作確認をしたい場合は以下の設定を行う。
 # listen 8080
 # ※「backlog」や「tcp_nopush」の設定もあるけど、よくわかって無い。
