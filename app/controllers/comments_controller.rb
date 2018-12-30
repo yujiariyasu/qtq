e08dcddf859ea19b5307bc894f36ad4831481518
yujiariyasu <yuji.ariyasu@gmail.com>
2018-12-30 17:43:21 +0900

    add edit comment function

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,12 @@ class CommentsController < ApplicationController
     @comment = Comment.create!(comment_params)
   end
 
+  def update
+    Comment.find(params[:id]).update(update_params)
+    flash[:info] = 'コメントを編集しました。'
+    redirect_to learning_url(params[:learning_id])
+  end
+
   def destroy
     Comment.find(params[:id]).destroy
     @comment_selector = "#comment#{params[:id]}"
@@ -11,5 +17,9 @@ class CommentsController < ApplicationController
   private
   def comment_params
     params.require(:comment).permit(:body).merge(learning_id: params[:learning_id], user_id: current_user.id)
+  end
+
+  def update_params
+    params.require(:comment).permit(:body)
   end
 end
