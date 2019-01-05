44443d12da59c820e67bc4b10fcaefbe8ea80ea9
yujiariyasu <yuji.ariyasu@gmail.com>
2019-01-06 06:24:03 +0900

    add precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,6 +2,7 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
+Rails.configuration.assets.precompile += %w(serviceworker.js manifest.json)
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
