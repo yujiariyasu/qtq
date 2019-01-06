8318d3b96e2aa47fa58012bca9ed0f5597258394
yujiariyasu <yuji.ariyasu@gmail.com>
2019-01-06 13:44:12 +0900

    goal設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -93,7 +93,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :avatar, :goal, :introduction)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :avatar, :introduction).merge(goal: 10)
   end
 
   def correct_user
