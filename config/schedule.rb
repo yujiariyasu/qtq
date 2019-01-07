a9dff98802392998801268fd070ddb3c3fbf1e4a
yujiariyasu <yuji.ariyasu@gmail.com>
2019-01-07 13:51:13 +0900

    test

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 set :output, 'log/crontab.log'
 set :environment, 'production'
 
-every 1.day, :at => ['4:30 am', '6:00 pm'] do
+every 1.day, :at => ['9:30 am', '8:00 pm'] do
   rake "webpush:send"
 end
