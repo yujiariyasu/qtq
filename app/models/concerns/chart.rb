703a41703dc0048bc14cfe1e79e1c2730ea12b22
yujiariyasu <yuji.ariyasu@gmail.com>
2019-03-03 20:38:25 +0900

    fix-review-chart3

--- app/models/concerns/chart.rb
+++ app/models/concerns/chart.rb
@@ -179,16 +179,17 @@ module Chart
 
   def review_chart(learning)
     review_data = [0, 100]
-    decrease_speed = learning.reviews.present? ? learning.proficiency_decrease_speed : INITIAL_DECREASE_SPEED
-    review_dates = learning.reviews.map{ |r| r.created_at.to_date }
+    review_date_proficiency_map = set_review_date_proficiency(learning)
+    decrease_speed = review_date_proficiency_map[learning.created_at.to_date].present? ? learning.proficiency_decrease_speed : INITIAL_DECREASE_SPEED
     end_date = learning.finished? ? learning.finish_date : Date.current
     range = chart_date_range(learning, end_date)
     date_category = range.to_a.map{ |date| "#{date}日目" }
-    if learning.created_at.to_date != Time.current.to_date
-      (learning.created_at.to_date.tomorrow..Time.now.to_date).each_with_index do |date, i|
+    if learning.created_at.to_date != Date.current
+      (learning.created_at.to_date.tomorrow..Date.current).each_with_index do |date, i|
         break if i >= date_category.size - 2
-        if review_dates.include?(date)
+        if review_date_proficiency_map.keys.include?(date)
           review_data << 100
+          decrease_speed = learning.calc_next_decrease_speed(decrease_speed, review_date_proficiency_map[date])
           next
         end
         last_data = review_data[-1] - decrease_speed
@@ -198,6 +199,14 @@ module Chart
     end
     text = learning.finished ? '忘れたらまた再開' : review_text(review_data[-1])
     return generate_review_chart(learning.title.truncate(100), text, date_category, review_data)
+  end
+
+  def set_review_date_proficiency(learning)
+    review_date_proficiency_map = {}
+    learning.reviews.each do |review|
+      review_date_proficiency_map[review.created_at.to_date] = review.proficiency
+    end
+    review_date_proficiency_map
   end
 
   # 経過日数が10以下の場合0..10を返す
